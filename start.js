ab0a60c1e4c62f2dd814e120e190a41f3ebf68b1
Maximo Rosso <maxrosso@uade.edu.ar>
2025-08-01 10:15:30 -0300

    FUNCIONAL

---
+++ start.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// Debug information
+console.log('Current working directory:', process.cwd());
+console.log('__dirname:', __dirname);
+console.log('__filename:', __filename);
+
+// Start the main application
+require('./index.js');
